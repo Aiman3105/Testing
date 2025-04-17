ca4b35eb5bf3d37d097aa9a57884b5be9818f675
Aiman <aimanelkhamis@yahoo.de>
2025-04-17 16:41:19 +0200

    camera_input

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import streamlit as st
-import cv2
-import time
-
 
 Button= st.button("Go")
 
 if Button:
-    video=cv2.VideoCapture(0)
-    check, frame = video.read()
-    cv2.imwrite("Test.png", frame)
-
-    st.image("Test.png")
+    st.camera_input("Camera")
